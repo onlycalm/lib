afdb978d92579674ff4681f6174b991b30172cff
chenpy <chenpy@maxeye.com>
2021-08-26 20:37:10 +0800

    style(Name): 修改log模块命名。
    1、修改log模块命名，使用缩写命名；
    2、修改log输出字符串格式；

--- log/log.h
+++ log/log.h
@@ -40,38 +40,38 @@
 #if LOG == ENABLE
 #define ToStr(x)                    Str(x)              //!< 宏值转字符串。
 #define Str(x)                      #x                  //!< 名转字符串。
-#define FmtLog(Lv)                  "[" Str(Lv) "] - [" __FILE__ " <" ToStr(__LINE__) ">](" __FUNCTION__ "): "
+#define FmtLog(Lv)                  "[" Str(Lv) "]-[" __FILE__ " <" ToStr(__LINE__) ">](" __FUNCTION__ "): "
 
 #if LOG_LV >= FATAL
-#define PrtLogFatal(Str, ...)       printf(FmtLog(FATAL) Str "\n", ##__VA_ARGS__)
+#define PrtLogFat(Str, ...)         printf(FmtLog(FATAL) Str "\n", ##__VA_ARGS__)
 #else
-#define PrtLogFatal()               ((void)0u)
+#define PrtLogFat()                 ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= ERROR
-#define PrtLogError(Str, ...)       printf(FmtLog(ERROR) Str "\n", ##__VA_ARGS__)
+#define PrtLogErr(Str, ...)         printf(FmtLog(ERROR) Str "\n", ##__VA_ARGS__)
 #else
-#define PrtLogError()               ((void)0u)
+#define PrtLogErr()                 ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= WARN
-#define PrtLogWarn(Str, ...)        printf(FmtLog(WARN) Str "\n", ##__VA_ARGS__)
+#define PrtLogWrn(Str, ...)         printf(FmtLog(WARN) Str "\n", ##__VA_ARGS__)
 #else
-#define PrtLogWarn()                ((void)0u)
+#define PrtLogWrn()                 ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= DEBUG
-#define PrtLogDebug(Str, ...)       printf(FmtLog(DEBUG) Str "\n", ##__VA_ARGS__)
+#define PrtLogDbg(Str, ...)         printf(FmtLog(DEBUG) Str "\n", ##__VA_ARGS__)
 #else
-#define PrtLogDebug()               ((void)0u)
+#define PrtLogDbg()                 ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= TRACE
-#define PrtLogTrace(Str, ...)       printf(FmtLog(TRACE) Str "\n", ##__VA_ARGS__)
+#define PrtLogTr(Str, ...)          printf(FmtLog(TRACE) Str "\n", ##__VA_ARGS__)
 #define PrtLogEntFun()              printf(FmtLog(TRACE) "<-----Enter function----->\n")
 #define PrtLogExFun()               printf(FmtLog(TRACE) "<-----Exit function----->\n")
 #else
-#define PrtLogTrace()               ((void)0u)
+#define PrtLogTr()                  ((void)0u)
 #define PrtLogEntFun()              ((void)0u)
 #define PrtLogExFun()               ((void)0u)
 #endif //LOG_LV
@@ -88,11 +88,11 @@
 
 #else
 #define FmtLog()                    ((void)0u)
-#define PrtLogFatal()               ((void)0u)
-#define PrtLogError()               ((void)0u)
-#define PrtLogWarn()                ((void)0u)
-#define PrtLogDebug()               ((void)0u)
-#define PrtLogTrace()               ((void)0u)
+#define PrtLogFat()                 ((void)0u)
+#define PrtLogErr()                 ((void)0u)
+#define PrtLogWrn()                 ((void)0u)
+#define PrtLogDbg()                 ((void)0u)
+#define PrtLogTr()                  ((void)0u)
 #define PrtLogEntFun()              ((void)0u)
 #define PrtLogExFun()               ((void)0u)
 #define PrtLogInfo()                ((void)0u)

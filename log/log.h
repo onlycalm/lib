a402d89fbac09bcb2e656e3e46d739344b87f3e3
onlycalm <741089410@qq.com>
2021-09-10 21:34:51 +0800

    feature(log): 规范化修改。
    1、修改log输出格式；
    2、修改log模块函数名；

--- log/log.h
+++ log/log.h
@@ -19,85 +19,85 @@
 //=============================================================================
 //配置宏
 #ifndef LOG
-#define LOG                         ENABLE              //!< Log模块开关。
+#define LOG                      ENABLE              //!< Log模块开关。
 #endif //LOG
 
 #ifndef LOG_LV
-#define LOG_LV                      3u                  //!< 默认Log等级。
+#define LOG_LV                   3u                  //!< 默认Log等级。
 #endif //LOG_LV
 
 //=============================================================================
 //Log等级
-#define FATAL                       5u                  //!< 致命。
-#define ERROR                       4u                  //!< 错误。
-#define WARN                        3u                  //!< 警告。
-#define DEBUG                       2u                  //!< 调试。
-#define TRACE                       1u                  //!< 跟踪。
-#define INFO                        0u                  //!< 信息。
+#define FATAL                    5u                  //!< 致命。
+#define ERROR                    4u                  //!< 错误。
+#define WARN                     3u                  //!< 警告。
+#define DEBUG                    2u                  //!< 调试。
+#define TRACE                    1u                  //!< 跟踪。
+#define INFO                     0u                  //!< 信息。
 
 //-----------------------------------------------------------------------------
 //宏函数
 #if LOG == ENABLE
-#define ToStr(x)                    Str(x)              //!< 宏值转字符串。
-#define Str(x)                      #x                  //!< 名转字符串。
-#define FmtLog(Lv)                  "[" Str(Lv) "]-[" __FILE__ " <" ToStr(__LINE__) ">](" __FUNCTION__ "): "
+#define ToStr(x)                 Str(x)              //!< 宏值转字符串。
+#define Str(x)                   #x                  //!< 名转字符串。
+#define FmtLog(Lv)               "[" Str(Lv) "]-(" __FILE__ " " __FUNCTION__ " " ToStr(__LINE__) "): "
 
 #if LOG_LV >= FATAL
-#define PrtLogFat(Str, ...)         printf(FmtLog(FATAL) Str "\n", ##__VA_ARGS__)
+#define LogFat(Str, ...)         printf(FmtLog(FATAL) Str "\n", ##__VA_ARGS__)
 #else
-#define PrtLogFat()                 ((void)0u)
+#define LogFat()                 ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= ERROR
-#define PrtLogErr(Str, ...)         printf(FmtLog(ERROR) Str "\n", ##__VA_ARGS__)
+#define LogErr(Str, ...)         printf(FmtLog(ERROR) Str "\n", ##__VA_ARGS__)
 #else
-#define PrtLogErr()                 ((void)0u)
+#define LogErr()                 ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= WARN
-#define PrtLogWrn(Str, ...)         printf(FmtLog(WARN) Str "\n", ##__VA_ARGS__)
+#define LogWrn(Str, ...)         printf(FmtLog(WARN) Str "\n", ##__VA_ARGS__)
 #else
-#define PrtLogWrn()                 ((void)0u)
+#define LogWrn()                 ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= DEBUG
-#define PrtLogDbg(Str, ...)         printf(FmtLog(DEBUG) Str "\n", ##__VA_ARGS__)
+#define LogDbg(Str, ...)         printf(FmtLog(DEBUG) Str "\n", ##__VA_ARGS__)
 #else
-#define PrtLogDbg()                 ((void)0u)
+#define LogDbg()                 ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= TRACE
-#define PrtLogTr(Str, ...)          printf(FmtLog(TRACE) Str "\n", ##__VA_ARGS__)
-#define PrtLogEntFun()              printf(FmtLog(TRACE) "<-----Enter function----->\n")
-#define PrtLogExFun()               printf(FmtLog(TRACE) "<-----Exit function----->\n")
+#define LogTr(Str, ...)          printf(FmtLog(TRACE) Str "\n", ##__VA_ARGS__)
+#define LogEntFun()              printf(FmtLog(TRACE) "<-----Enter function----->\n")
+#define LogExFun()               printf(FmtLog(TRACE) "<-----Exit function----->\n")
 #else
-#define PrtLogTr()                  ((void)0u)
-#define PrtLogEntFun()              ((void)0u)
-#define PrtLogExFun()               ((void)0u)
+#define LogTr()                  ((void)0u)
+#define LogEntFun()              ((void)0u)
+#define LogExFun()               ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= INFO
-#define PrtLogInfo(Str, ...)        printf(FmtLog(INFO) Str "\n", ##__VA_ARGS__)
-#define PrtLogRtn(Rtn)              printf(FmtLog(INFO) "%s: %d\n", #Rtn, Rtn)
-#define PrtLogVar(Var)              printf(FmtLog(INFO) "%s: %d\n", #Var, Var)
+#define LogInfo(Str, ...)        printf(FmtLog(INFO) Str "\n", ##__VA_ARGS__)
+#define LogRtn(Rtn)              printf(FmtLog(INFO) "%s: %d\n", #Rtn, Rtn)
+#define LogVar(Var)              printf(FmtLog(INFO) "%s: %d\n", #Var, Var)
 #else
-#define PrtLogInfo()                ((void)0u)
-#define PrtLogRtn()                 ((void)0u)
-#define PrtLogVar()                 ((void)0u)
+#define LogInfo()                ((void)0u)
+#define LogRtn()                 ((void)0u)
+#define LogVar()                 ((void)0u)
 #endif //LOG_LV
 
 #else
-#define FmtLog()                    ((void)0u)
-#define PrtLogFat()                 ((void)0u)
-#define PrtLogErr()                 ((void)0u)
-#define PrtLogWrn()                 ((void)0u)
-#define PrtLogDbg()                 ((void)0u)
-#define PrtLogTr()                  ((void)0u)
-#define PrtLogEntFun()              ((void)0u)
-#define PrtLogExFun()               ((void)0u)
-#define PrtLogInfo()                ((void)0u)
-#define PrtLogRtn()                 ((void)0u)
-#define PrtLogVar()                 ((void)0u)
+#define FmtLog()                 ((void)0u)
+#define LogFat()                 ((void)0u)
+#define LogErr()                 ((void)0u)
+#define LogWrn()                 ((void)0u)
+#define LogDbg()                 ((void)0u)
+#define LogTr()                  ((void)0u)
+#define LogEntFun()              ((void)0u)
+#define LogExFun()               ((void)0u)
+#define LogInfo()                ((void)0u)
+#define LogRtn()                 ((void)0u)
+#define LogVar()                 ((void)0u)
 #endif //LOG
 
 #endif //LOG_H

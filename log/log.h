aa7558c7985a7e42b33c433360a5d05d1ea28286
onlycalm <741089410@qq.com>
2021-09-14 15:04:40 +0800

    feature(log): log模块规范化修改。
    1、log模块日志等级做了优化，参考python中loguru模块设计；

--- log/log.h
+++ log/log.h
@@ -23,17 +23,18 @@
 #endif //LOG
 
 #ifndef LOG_LV
-#define LOG_LV                   3u                  //!< 默认Log等级。
+#define LOG_LV                   TRACE               //!< 默认Log等级。
 #endif //LOG_LV
 
 //=============================================================================
 //Log等级
-#define FATAL                    5u                  //!< 致命。
-#define ERROR                    4u                  //!< 错误。
-#define WARN                     3u                  //!< 警告。
-#define DEBUG                    2u                  //!< 调试。
-#define TRACE                    1u                  //!< 跟踪。
-#define INFO                     0u                  //!< 信息。
+#define CRITICAL                 50u                 //!< 致命。
+#define ERROR                    40u                 //!< 错误。
+#define WARNING                  30u                 //!< 警告。
+#define SUCCESS                  25u                 //!< 成功。
+#define INFO                     20u                 //!< 信息。
+#define DEBUG                    10u                 //!< 调试。
+#define TRACE                    5u                  //!< 跟踪。
 
 //-----------------------------------------------------------------------------
 //宏函数
@@ -42,8 +43,8 @@
 #define Str(x)                   #x                  //!< 名转字符串。
 #define FmtLog(Lv)               "[" Str(Lv) "] " __FILE__ ":" __FUNCTION__ ":" ToStr(__LINE__) " - "
 
-#if LOG_LV >= FATAL
-#define LogFat(Str, ...)         printf(FmtLog(FATAL) Str "\n", ##__VA_ARGS__)
+#if LOG_LV >= CRITICAL
+#define LogFat(Str, ...)         printf(FmtLog(CRITICAL) Str "\n", ##__VA_ARGS__)
 #else
 #define LogFat()                 ((void)0u)
 #endif //LOG_LV
@@ -54,10 +55,22 @@
 #define LogErr()                 ((void)0u)
 #endif //LOG_LV
 
-#if LOG_LV >= WARN
-#define LogWrn(Str, ...)         printf(FmtLog(WARN) Str "\n", ##__VA_ARGS__)
+#if LOG_LV >= WARNING
+#define LogWrn(Str, ...)         printf(FmtLog(WARNING) Str "\n", ##__VA_ARGS__)
 #else
 #define LogWrn()                 ((void)0u)
+#endif //LOG_LV
+
+#if LOG_LV >= SUCCESS
+#define LogSuc(Str, ...)         printf(FmtLog(SUCCESS) Str "\n", ##__VA_ARGS__)
+#else
+#define LogSuc()                 ((void)0u)
+#endif //LOG_LV
+
+#if LOG_LV >= INFO
+#def ine LogInf(Str, ...)        printf(FmtLog(INFO) Str "\n", ##__VA_ARGS__)
+#else
+#define LogInf ()                ((void)0u)
 #endif //LOG_LV
 
 #if LOG_LV >= DEBUG
@@ -68,36 +81,19 @@
 
 #if LOG_LV >= TRACE
 #define LogTr(Str, ...)          printf(FmtLog(TRACE) Str "\n", ##__VA_ARGS__)
-#define LogEntFun()              printf(FmtLog(TRACE) "<-----Enter function----->\n")
-#define LogExFun()               printf(FmtLog(TRACE) "<-----Exit function----->\n")
 #else
 #define LogTr()                  ((void)0u)
-#define LogEntFun()              ((void)0u)
-#define LogExFun()               ((void)0u)
-#endif //LOG_LV
-
-#if LOG_LV >= INFO
-#define LogInfo(Str, ...)        printf(FmtLog(INFO) Str "\n", ##__VA_ARGS__)
-#define LogRtn(Rtn)              printf(FmtLog(INFO) "%s: %d\n", #Rtn, Rtn)
-#define LogVar(Var)              printf(FmtLog(INFO) "%s: %d\n", #Var, Var)
-#else
-#define LogInfo()                ((void)0u)
-#define LogRtn()                 ((void)0u)
-#define LogVar()                 ((void)0u)
 #endif //LOG_LV
 
 #else
 #define FmtLog()                 ((void)0u)
-#define LogFat()                 ((void)0u)
+#define LogCrt()                 ((void)0u)
 #define LogErr()                 ((void)0u)
 #define LogWrn()                 ((void)0u)
+#define LogScs()                 ((void)0u)
+#define LogInf()                 ((void)0u)
 #define LogDbg()                 ((void)0u)
 #define LogTr()                  ((void)0u)
-#define LogEntFun()              ((void)0u)
-#define LogExFun()               ((void)0u)
-#define LogInfo()                ((void)0u)
-#define LogRtn()                 ((void)0u)
-#define LogVar()                 ((void)0u)
 #endif //LOG
 
 #endif //LOG_H
